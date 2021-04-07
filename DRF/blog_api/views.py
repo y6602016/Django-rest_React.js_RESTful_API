42f0333dc240cd4ea70b853d07236f68e747060e
Qi-Rui Hong <y6602016@gmail.com>
2021-04-07 11:11:40 +0800

    update permission

--- DRF/blog_api/views.py
+++ DRF/blog_api/views.py
@@ -1,12 +1,24 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAdminUser, DjangoModelPermissions, IsAuthenticatedOrReadOnly
+
+class PostUserWritePermission(BasePermission):
+    message = 'Editing posts is retricted to the author only.'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        # like a filter to return the post whose auth is the request user    
+        return obj.author == request.user
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
