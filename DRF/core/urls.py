0e28f32e9998b092e257e9f8022812317051a464
mike <y6602016@gmail.com>
2021-04-08 16:56:53 +0800

    add API documentation

--- DRF/core/urls.py
+++ DRF/core/urls.py
@@ -1,20 +1,7 @@
-"""core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -28,4 +15,10 @@ urlpatterns = [
     path('api/user/', include('users.urls', namespace="users")),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('blog.urls', namespace='blog')), 
+    path('docs/', include_docs_urls(title='BlogAPI')),
+    path('schema', get_schema_view(
+        title="Blog API",
+        description="API for the BlogAPI",
+        version="1.0.0"
+    ), name="openapi-schma"),
 ]
